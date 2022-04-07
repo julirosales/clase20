67054075cd785fedaa3be53b37428c3f9163fe95
Julieta Rosales <julietarosalesagostina@gmail.com>
2022-04-07 18:14:20 -0300

    agrego al objeto render

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,11 +1,14 @@
+//destructuracion de objetos es como hacer dos const
+const {about , listaPlatos} = require('./info')
+
 const mainController = {
   index: (req, res) => {
-    res.render("index");
+    res.render("index",{about: about , menu:listaPlatos});//llamo a los objetos para poder inyectarlos por template en el index
   },
 
   detalle: (req, res) => {
-    console.log(plato);
-    res.render("detalleMenu");
+   let plato = listaPlatos.find((plato)=> plato.id == req.params.menuId)
+    res.render("detalleMenu", {plato:plato});
   },
 };
 
